718cecb83730f5fa0c156d22a250723d5337901c
Ginés López <docentegineslopez@gmail.com>
2026-08-07 11:49:09 +0200

    Cambio de fuentes

--- astro.config.mjs
+++ astro.config.mjs
@@ -15,26 +15,20 @@ export default defineConfig({
 	],
 	fonts: [
 		{
-			provider: fontProviders.local(),
-			name: 'Atkinson',
-			cssVariable: '--font-atkinson',
+			provider: fontProviders.google(),
+			name: 'Inter',
+			cssVariable: '--font-inter',
 			fallbacks: ['sans-serif'],
-			options: {
-				variants: [
-					{
-						src: ['./src/assets/fonts/atkinson-regular.woff'],
-						weight: 400,
-						style: 'normal',
-						display: 'swap',
-					},
-					{
-						src: ['./src/assets/fonts/atkinson-bold.woff'],
-						weight: 700,
-						style: 'normal',
-						display: 'swap',
-					},
-				],
-			},
+			weights: [400, 500, 600, 700, 800],
+			styles: ['normal'],
+		},
+		{
+			provider: fontProviders.google(),
+			name: 'Roboto',
+			cssVariable: '--font-roboto',
+			fallbacks: ['sans-serif'],
+			weights: [400, 500, 700],
+			styles: ['normal', 'italic'],
 		},
 	],
 });
